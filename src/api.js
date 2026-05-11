a09aed4060c8f80ae8e14b4cb2d8c8de16c7597e
Filip <tjofee47@gmail.com>
2026-05-11 15:02:17 -0600

    Make API calls better and parse them

---
+++ src/api.js
@@ -0,0 +1,31 @@
+async function callWeatherApi(location) {
+    const apiKey = process.env.WEBPACK_API_KEY;
+    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=current,hours&key=${apiKey}`;
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return await response.json();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function getWeatherData(location) {
+    const apiData = await callWeatherApi(location);
+    let processedWeatherData = {
+        location: apiData.resolvedAddress,
+        tempCurrent: apiData.currentConditions.temp,
+        tempMin: apiData.days[0].tempmin,
+        tempMax: apiData.days[0].tempmax,
+        humidity: apiData.currentConditions.humidity,
+        feelsLike: apiData.currentConditions.feelslike,
+        windSpeed: apiData.currentConditions.windspeed,
+        windGust: apiData.currentConditions.windgust,
+        currentConditions: apiData.currentConditions.conditions,
+        dailyConditions: apiData.days[0].description,
+        icon: apiData.currentConditions.icon,
+    }
+    return processedWeatherData;
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import "./styles.css"
+import { getWeatherData } from "./api.js"
 
-async function weatherApiCall(location) {
-    const apiKey = process.env.WEBPACK_API_KEY;
-    console.log(apiKey)
-    const apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&include=days,current,hours&key=${apiKey}`;
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        const weatherDataResponse = await response.json();
-        return weatherDataResponse;
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-const weatherData = await weatherApiCall('denver');
-console.log(weatherData);
+const test = await getWeatherData('longmont')
+console.log(test)
