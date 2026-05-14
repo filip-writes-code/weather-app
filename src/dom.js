4e4a0bcf197b2010b1256a093264a80c0a59060c
Filip <tjofee47@gmail.com>
2026-05-14 12:08:36 -0600

    Dom render

---
+++ src/dom.js
@@ -0,0 +1,17 @@
+const cityName = document.querySelector(".city");
+const currentTemp = document.querySelector(".current-temp");
+const conditions = document.querySelector(".conditions");
+const high = document.querySelector(".high");
+const low = document.querySelector(".low");
+const windSpeed = document.querySelector(".wind-speed");
+const windGusts = document.querySelector(".wind-gusts");
+
+export function renderWeather(dataObject) {
+  cityName.textContent = dataObject.city;
+  currentTemp.textContent = dataObject.tempCurrent;
+  conditions.textContent = dataObject.currentConditions;
+  high.textContent = `High: ${dataObject.tempMax}`;
+  low.textContent = `Low: ${dataObject.tempMin}`;
+  windSpeed.textContent = `Wind: ${dataObject.windSpeed}`;
+  windGusts.textContent = `Wind: ${dataObject.windGust}`;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./styles.css";
 import { getWeatherData } from "./api.js";
+import { renderWeather } from "./dom.js";
 
-const test = await getWeatherData("longmont");
+const test = await getWeatherData("Boulder CO");
 console.log(test);
+renderWeather(test);
